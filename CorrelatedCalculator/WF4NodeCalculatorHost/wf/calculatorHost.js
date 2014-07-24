6c4323c70552eb69b6f08efc88e96b779b15dd12
gmezo <gabor.mezo@swicon.hu>
2014-07-24 17:16:58 +0200

    test updated

--- CorrelatedCalculator/WF4NodeCalculatorHost/wf/calculatorHost.js
+++ CorrelatedCalculator/WF4NodeCalculatorHost/wf/calculatorHost.js
@@ -5,11 +5,15 @@ var connStr = "mongodb://swiiis01/calculator";
 var WorkflowHost = require("workflow-4-node").hosting.WorkflowHost;
 var MongoDDPersistence = require("workflow-4-node").hosting.mongoDB.MongoDDPersistence;
 
+var MemoryPersistence = require("workflow-4-node").hosting.MemoryPersistence;
+
 var persistence = new MongoDDPersistence({
     connection: connStr
 });
 
 //var persistence = null;
+
+//var persistence = new MemoryPersistence();
 
 var host = new WorkflowHost(
     {
